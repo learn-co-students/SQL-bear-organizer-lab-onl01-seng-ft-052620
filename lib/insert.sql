8c1ff524aec4ec256b39bb2fd980c3b5b32c94f6
aaronfeingold <ajfeingold88@gmail.com>
2020-06-22 20:05:30 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,18 @@
+-- INSERT INTO bears VALUES (1, "Mr.Chocolate", 10, "M", "black", "chill", 1);
+-- INSERT INTO bears VALUES (2, "Rowdy", 12, "M", "brown", "calm", 1);
+-- INSERT INTO bears VALUES (3, "Tabitha", 8, "F", "white", "happy", 0);
+-- INSERT INTO bears VALUES (4, "Sergeant Brown", 11, "M", "brown", "crazy", 1);
+-- INSERT INTO bears VALUES (5, "Melissa", 21, "F", "black", "agressive", 1);
+-- INSERT INTO bears VALUES (6, "Grinch", 15, "M", "white", "calm", 0);
+-- INSERT INTO bears VALUES (7, "Wendy", 7, "F", "red", "chill", 1);
+-- INSERT INTO bears VALUES (8, null, 3, "M", "brown", "crazy", 1);
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+(1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
+(2,"Rowdy", 10, "M", "black", "intense", 1),
+(3,"Tabitha", 6, "F", "dark brown", "Nice", 1),
+(4,"Sargeant Brown", 19, "M", "Green", "Slimy", 0),
+(5,"Melissa", 13, "F", "dark brown", "goofy", 1),
+(6,"Grinch", 2, "M", "Black", "Grinchy", 1),
+(7,"Wendy", 6, "F", "Blue", "naive", 1),
+(8,null, 20, "M", "black", "aggressive", 0);
