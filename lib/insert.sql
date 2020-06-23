3c8965f8783ad98b7e5a477de3989cf48ed4dddf
SWFullPotential <swfullpotential@gmail.com>
2020-06-22 19:42:46 -0600

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+ VALUES ("Mr. Chocolate", 6, "male", "brown", "hungry", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+ VALUES ("Rowdy", 4, "male", "brown", "rambunctious", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+ VALUES ("Tabitha", 3, "female", "black", "sweet", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+ VALUES ("Sergeant Brown", 9, "male", "black", "stoic", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+ VALUES ("Melissa", 2, "female", "brown", "hungry", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+ VALUES ("Grinch", 7, "male", "black", "grumpy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+ VALUES ("Wendy", 4, "female", "brown", "tired", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+ VALUES (NULL, 9, "male", "black", "protective", 0);
