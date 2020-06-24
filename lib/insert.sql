990e0933f7a2c317d70a2edf2ac3e26e600dbae3
barrettanderson <barrettanderson1989@gmail.com>
2020-06-23 17:20:59 -0700

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 7, "male", "brown", "full", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 2, "male", "sandy", "rambunxious", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 14, "female", "chartreuse", "wise", 0);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 13, "female", "white", "honorable", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 5, "female", "black", "insightful", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 17, "male", "green", "growing", 0);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 18, "female", "tan", "struggling", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 7, "M", "brown", "full", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 2, "M", "sandy", "rambunxious", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 14, "F", "chartreuse", "wise", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 13, "F", "white", "honorable", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 5, "F", "black", "insightful", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 17, "M", "green", "growing", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 18, "F", "brown", "struggling", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, NULL, NULL, NULL, NULL, 0);
